2217f558a3fe7ba6e63128fe21251489aa04770f
Dmitry <misteranonimus555@gmail.com>
2022-06-18 15:56:48 +0300

    линтер, на фронте вынесен baseURL в отдельный конфиг

--- server/schemas/admin.ts
+++ server/schemas/admin.ts
@@ -1,15 +1,14 @@
-import {Schema} from 'mongoose'
-import * as mongoose from 'mongoose'
+import {Schema} from 'mongoose';
+import * as mongoose from 'mongoose';
 
-const AdminSchema = new  Schema({
-    login:{type: String, required: true},
-    password:{type: String, required: true},
-})
+const AdminSchema = new Schema({
+  login: {type: String, required: true},
+  password: {type: String, required: true},
+});
 module.exports=mongoose.model<IAdmin>('AdminSchema', AdminSchema);
 
 
-
-export interface IAdmin  extends mongoose.Document{
+export interface IAdmin extends mongoose.Document{
     login:string
     password:string,
 }
--- server/schemas/info.ts
+++ server/schemas/info.ts
@@ -1,18 +1,18 @@
-import {Schema} from 'mongoose'
-import * as mongoose from 'mongoose'
+import {Schema} from 'mongoose';
+import * as mongoose from 'mongoose';
 
-const InfoSchema = new  Schema({
-    image:{type: String, required: true},
-    name:{type: String, required: true},
-    job:{type: String, required: true},
-    desc:{type: String, required: true},
-    workWidthTittle:{type: String, required: true},
-    workWidth:{type: String, required: true},
-    contacts: [{type: Object }],
-})
+const InfoSchema = new Schema({
+  image: {type: String, required: true},
+  name: {type: String, required: true},
+  job: {type: String, required: true},
+  desc: {type: String, required: true},
+  workWidthTittle: {type: String, required: true},
+  workWidth: {type: String, required: true},
+  contacts: [{type: Object}],
+});
 module.exports=mongoose.model<IInfo>('InfoSchema', InfoSchema);
 
-export interface IInfo  extends mongoose.Document{
+export interface IInfo extends mongoose.Document{
     image:string
     name:string
     desc:string,
