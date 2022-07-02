a0956cc578d52b44dc2011c099504d5bb05b6b8c
ditras228 <misteranonimus555@gmail.com>
2022-07-02 04:56:09 +0300

    прибран репозиторий, стили, гит игнор

--- client/src/vendor.ts
+++
@@ -1,7 +0,0 @@
-export const isEmpty = (obj: any) => {
-  for (let key in obj) {
-    // если тело цикла начнет выполняться - значит в объекте есть свойства
-    return false;
-  }
-  return true;
-};
