624207da88c234ff27451af2fdb741a6579801d6
ditras228 <misteranonimus555@gmail.com>
2022-08-06 23:28:32 +0300

    делпой

--- client-angular/src/app/config/appConfig.ts
+++ client-angular/src/app/config/appConfig.ts
@@ -1,5 +1,5 @@
 const AppConfig = {
-  baseUrl: 'http://192.168.1.2:8080/grapql',
-  imgUrl: 'http://192.168.1.2:4001',
+  baseUrl: 'http://5.181.108.248:8080/grapql',
+  imgUrl: 'http://5.181.108.248:4001',
 };
 export default AppConfig;
